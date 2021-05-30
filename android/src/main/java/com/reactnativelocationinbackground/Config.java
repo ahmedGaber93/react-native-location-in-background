f121c124e5a619ec6157157406b03cc1f4fb55c3
ahmedGaber93 <a.gaber0093@gmail.com>
2021-05-30 16:29:10 +0200

    update android

--- android/src/main/java/com/reactnativelocationinbackground/Config.java
+++ android/src/main/java/com/reactnativelocationinbackground/Config.java
@@ -5,9 +5,13 @@ import android.location.Location;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.TimeZone;
 
 
 public class Config {
@@ -135,11 +139,25 @@ public class Config {
             extraPostData.put(getParamName("accuracy"), lastLocation.getAccuracy());
             extraPostData.put(getParamName("altitude"), lastLocation.getAltitude());
             extraPostData.put(getParamName("speed"), lastLocation.getSpeed());
-            extraPostData.put(getParamName("time"), lastLocation.getTime());
+            extraPostData.put(getParamName("time"), getDateCurrentTimeZone(lastLocation.getTime()));
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return extraPostData;
+    }
+
+    public String getDateCurrentTimeZone(long timestamp) {
+        try{
+            Calendar calendar = Calendar.getInstance();
+            TimeZone tz = TimeZone.getDefault();
+            calendar.setTimeInMillis(timestamp * 1000);
+            calendar.add(Calendar.MILLISECOND, tz.getOffset(calendar.getTimeInMillis()));
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date currenTimeZone = (Date) calendar.getTime();
+            return sdf.format(currenTimeZone);
+        }catch (Exception e) {
+        }
+        return "";
     }
 
 
